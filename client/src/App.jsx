ac7ed8bfc3e03631b24c64eb6a6d4e60fc6f4161
Ayush Sharma <ayush.sharma74892@gmail.com>
2024-06-18 06:36:36 +0000

    Initialised react project

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+
+
+  return (
+    <h1 className="text-center text-4xl">Stream Pulse</h1>
+  )
+}
+
+export default App
--- src/app/page.jsx
+++
@@ -1,7 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <h1 className="text-center text-4xl">Youtube Clone</h1>
-  );
-}
